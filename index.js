c88f1558528ac50415dcc69c41f6e0ddb6cb73c7
dips98 <upadhayay.dipesh@gmail.com>
2024-05-01 20:08:45 +0530

    Devloped register

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors())
+const port = 3000;
+
 const jsonParser = bodyParser.json();
 // app.use(bodyParser.json());
 
@@ -9,14 +14,21 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 mongoose.connect('mongodb+srv://dipesh:Next%40123@cluster0.flcyeme.mongodb.net/ecom')
-  .then(() => console.log('DB Connected!'));
+  .then(() => console.log('DB Connected!'))
+  .catch((e) => {
+    console.log("Data base not connected! :", e);
+  })
 
 
 const userSchema = new Schema({
   firstname: String,
   lastname: String,
   username: String,
-  email: String
+  email: String,
+  password: String,
+  confirmPassword: String,
+  username: String,
+  contact: String,
 });
 
 const User = mongoose.model('User', userSchema);
@@ -29,20 +41,20 @@ app.post('/register', jsonParser, (req, res) => {
 
   console.log("Body data", req.body);
 
-  const { firstname, lastname, username, email } = req.body;
+  const { firstName, lastName, userName, email, password, confirmPassword, contact } = req.body;
   const createNewUser = new User({
-    firstname: firstname,
-    lastname: lastname,
-    username: username,
-    email: email
+    firstname: firstName,
+    lastname: lastName,
+    contact: contact,
+    email: email,
+    password: password,
+    confirmPassword: confirmPassword,
+    username: userName,
   })
 
   createNewUser.save().then((result) => {
-    res.status(201).json({ msg: 'New Created successfully!', result: result });
+    res.status(201).json({ msg: 'New user created successfully!', result: result });
   })
-
-  // res.send('Register API')
-
 
 })
 
